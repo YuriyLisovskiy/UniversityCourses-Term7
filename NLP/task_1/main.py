0a0d802eec0a07bb4df9eaa2d66d221802bc5682
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-11-30 01:12:53 +0200

    minor improvements

--- NLP/task_1/main.py
+++ NLP/task_1/main.py
@@ -1,48 +1,19 @@
-import json
-from google.api_core.exceptions import InvalidArgument
-
-from monad.io import IO
 from app.settings import (
 	DIALOGFLOW_PROJECT_ID,
 	DIALOGFLOW_LANGUAGE_CODE,
 	TELEGRAM_BOT_TOKEN
 )
-from df_agent.agent import Agent, get_sessions_client
-
 from t_bot.bot import UniversityAssistanceBot
-
-config = {
-	'project_id': DIALOGFLOW_PROJECT_ID,
-	'lang_code': DIALOGFLOW_LANGUAGE_CODE,
-	'client': get_sessions_client()
-}
-
-
-def print_dict(d):
-	print(json.dumps(d, sort_keys=True, indent=4))
-
-
-def interact(question, agent):
-	try:
-		# print_dict(agent.detect_intent(question))
-		resp = agent.detect_intent(question)
-		print('Bot[{}]:'.format(resp['detected_intent']), resp['fulfillment_text'])
-	except InvalidArgument:
-		raise
-
-
-def step(agent):
-	return IO.print('You: ', end='').bind(
-		lambda _: IO.read().bind(
-			lambda q: IO.ret(interact(q, agent)).bind(
-				lambda _: step(agent)
-			) if q != '/stop' else IO.ret(None)
-		)
-	)
+from df_agent.agent import get_sessions_client
 
 
 if __name__ == '__main__':
-	# a = Agent(**config)
-	# step(a).run()
-	bot = UniversityAssistanceBot(TELEGRAM_BOT_TOKEN, df_config=config)
-	bot.start()
+	config = {
+		'project_id': DIALOGFLOW_PROJECT_ID,
+		'lang_code': DIALOGFLOW_LANGUAGE_CODE,
+		'client': get_sessions_client()
+	}
+	UniversityAssistanceBot(
+		token=TELEGRAM_BOT_TOKEN,
+		df_config=config
+	).start()
