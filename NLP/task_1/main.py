77289a6ee5e5d826be36d12702b53c0329698981
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-11-30 01:25:25 +0200

    minor changes

--- NLP/task_1/main.py
+++ NLP/task_1/main.py
@@ -1,19 +1,15 @@
-from app.settings import (
-	DIALOGFLOW_PROJECT_ID,
-	DIALOGFLOW_LANGUAGE_CODE,
-	TELEGRAM_BOT_TOKEN
-)
-from t_bot.bot import UniversityAssistanceBot
+from app import settings as s
+from app.bot import UniversityAssistanceBot
 from df_agent.agent import get_sessions_client
 
 
 if __name__ == '__main__':
 	config = {
-		'project_id': DIALOGFLOW_PROJECT_ID,
-		'lang_code': DIALOGFLOW_LANGUAGE_CODE,
+		'project_id': s.DIALOGFLOW_PROJECT_ID,
+		'lang_code': s.DIALOGFLOW_LANGUAGE_CODE,
 		'client': get_sessions_client()
 	}
 	UniversityAssistanceBot(
-		token=TELEGRAM_BOT_TOKEN,
+		token=s.TELEGRAM_BOT_TOKEN,
 		df_config=config
 	).start()
